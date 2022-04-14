811b12d3e09f4ac0be01ae8a9897e65d8ea40a1d
Justin Priest <justin.priest@alaska.gov>
2022-04-14 12:03:28 -0800

    adding first function

---
+++ R/statweek.R
@@ -0,0 +1,17 @@
+
+#' Calculate the Statistical Week based on a date object
+#'
+#' @param x The date column of interest to convert
+#'
+#' @keywords stat week
+#' @return
+#' @export
+#'
+#' @examples
+#' dataframe %>% mutate(week = statweek(datecolumn))
+statweek <- function(x) {
+  as.numeric(format(as.Date(x), "%U")) - as.numeric(format(as.Date(cut(x, "year")), "%U")) + 1
+  # Function modified from:
+  # https://stackoverflow.com/questions/17286983/calculate-statistical-week-starting-1st-january-as-used-in-fisheries-data
+}
+
